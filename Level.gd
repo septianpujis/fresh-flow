084838cd1f5676e526cd9bca9b526f7318a13281
septianpujis <septianpujipuji@gmail.com>
2023-03-24 13:44:45 +0700

    experiment on level generate

--- Level.gd
+++ Level.gd
@@ -3,7 +3,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var poli = $Path2D.curve.get_baked_points()
-	$StaticBody2D/CollisionPolygon2D.polygon = poli
-	$Polygon2D.polygon = poli
-	$Line2D.points = poli
+	var poliBottom = $Bottom/Path2D.curve.get_baked_points()
+	$Bottom/StaticBody2D/CollisionPolygon2D.polygon = poliBottom
+	$Bottom/Polygon2D.polygon = poliBottom
+	$Bottom/Line2D.points = poliBottom
+	
+	var poliTop = $Top/Path2D.curve.get_baked_points()
+	$Top/StaticBody2D/CollisionPolygon2D.polygon = poliTop
+	$Top/Polygon2D.polygon = poliTop
+	$Top/Line2D.points = poliTop
